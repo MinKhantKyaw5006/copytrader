16b33772964699ebe714902eff4ce5e5b45a4fa8
MinKhantKyaw <minkhant050603@gmail.com>
2025-04-20 02:14:36 +0700

    add sonner and add config

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+const config ={
+    env: {
+        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+        prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
+    }
+}
+export default config;
