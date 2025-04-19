67b238c2a446a7e482bd9ff6004bb93e8741d57a
MinKhantKyaw <minkhant050603@gmail.com>
2025-04-20 03:23:15 +0700

    test fetch

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 import { Button } from "@/components/ui/button"
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = ()=>(
+const Home = async ()=>{
+    const result = await db.select().from(users);
+    console.log(JSON.stringify(result, null, 2));
+    return(
+        <>
+        <Button>Click Me</Button>
+        </>
+    )
 
-    <>
-    <Button>Click Me</Button>
-    </>
+}
+
+
   
-);
+
 
 export default Home;
